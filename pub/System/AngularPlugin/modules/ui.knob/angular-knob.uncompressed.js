ed7c2a16e51efd92a24ec3b76399718e4af519b0
MichaelDaum <github@michaeldaumconsulting.com>
2014-09-22 11:26:39 +0200

    Item13033: js-beautify

--- pub/System/AngularPlugin/modules/ui.knob/angular-knob.uncompressed.js
+++ pub/System/AngularPlugin/modules/ui.knob/angular-knob.uncompressed.js
@@ -1,37 +1,37 @@
 angular.module('ui.knob', [])
-  .directive('knob', function () {
+  .directive('knob', function() {
     return {
       restrict: 'EACM',
-      template: function(elem, attrs){
+      template: function(elem, attrs) {
 
         return '<input value="{{ knob }}">';
 
       },
       replace: true,
       require: 'ngModel',
-      link: function (scope, elem, attrs, ngModel) {
+      link: function(scope, elem, attrs, ngModel) {
 
-          var opts = {}; 
+        var opts = {};
 
-          if(!angular.isUndefined(attrs.max)) {
-            opts.max = attrs.max;
-          }
+        if (!angular.isUndefined(attrs.max)) {
+          opts.max = attrs.max;
+        }
 
-          if(!angular.isUndefined(attrs.fgColor)) {
-            opts.fgColor = attrs.fgColor;
-          }
+        if (!angular.isUndefined(attrs.fgColor)) {
+          opts.fgColor = attrs.fgColor;
+        }
 
-          opts.change = function(value) {
-            scope.$apply(function() {
-              ngModel.$setViewValue(value);
-            });
-          }
+        opts.change = function(value) {
+          scope.$apply(function() {
+            ngModel.$setViewValue(value);
+          });
+        }
 
-          elem.knob(opts);
+        elem.knob(opts);
 
-          ngModel.$render = function() {
-            elem.val(ngModel.$viewValue).trigger("change");
+        ngModel.$render = function() {
+          elem.val(ngModel.$viewValue).trigger("change");
         };
       }
     };
-});
+  });
